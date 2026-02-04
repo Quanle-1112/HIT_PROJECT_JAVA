fd4db9ac98d33bf2ab2d7a5e425b4f69ca216c48
Quanle-1112 <lecongquan.dtan@gmail.com>
2026-02-05 01:19:07 +0700

    feat: demo favorite list function

--- HIT_PROJECT_JAVA/src/main/java/org/example/controllers/favorite/FavoriteItemController.java
+++ HIT_PROJECT_JAVA/src/main/java/org/example/controllers/favorite/FavoriteItemController.java
@@ -1,5 +1,6 @@
 package org.example.controllers.favorite;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -22,44 +23,74 @@ public class FavoriteItemController {
     @FXML private HBox itemContainer;
     @FXML private ImageView bookThumb;
     @FXML private Label bookTitle;
+    @FXML private Label statusLabel;
     @FXML private Button btnRemove;
 
     private final FavoriteDAO favoriteDAO = new FavoriteDAO();
     private UserFavorite currentFav;
 
-    public void setData(UserFavorite fav) {
+    private final String IMAGE_BASE_URL = "https://img.otruyenapi.com/uploads/comics/";
+
+    public void setData(UserFavorite fav, Runnable onDeleteCallback) {
         this.currentFav = fav;
 
         bookTitle.setText(fav.getBookName());
 
-        String fullUrl = "https://img.otruyenapi.com/uploads/comics/" + fav.getThumbnailUrl();
-        ImageLoaderGlobal.setImage(fullUrl, bookThumb);
+        String dateAdded = "";
+        if (fav.getAddedAt() != null) {
+            dateAdded = fav.getAddedAt().toString();
+            if (dateAdded.length() > 10) dateAdded = dateAdded.substring(0, 10);
+        }
+        if (statusLabel != null) {
+            statusLabel.setText("Đã thêm: " + dateAdded);
+        }
 
-        itemContainer.setOnMouseClicked(e -> {
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/read/book_detail.fxml"));
-                Parent root = loader.load();
+        String rawThumb = fav.getThumbnailUrl();
+        String finalUrl;
+        if (rawThumb == null || rawThumb.isEmpty()) {
+            finalUrl = "";
+        } else if (rawThumb.startsWith("http")) {
+            finalUrl = rawThumb;
+        } else {
+            finalUrl = IMAGE_BASE_URL + rawThumb;
+        }
+        ImageLoaderGlobal.setImage(finalUrl, bookThumb);
 
-                BookDetailController detailController = loader.getController();
-                detailController.setBookSlug(fav.getBookSlug());
-
-                Stage stage = (Stage) itemContainer.getScene().getWindow();
-                stage.setScene(new Scene(root));
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        });
+        itemContainer.setOnMouseClicked(e -> openBookDetail());
 
         if (btnRemove != null) {
             btnRemove.setOnAction(e -> {
-                boolean success = favoriteDAO.removeFavorite(fav.getUserId(), fav.getBookSlug());
-                if (success) {
-                    VBox parent = (VBox) itemContainer.getParent();
-                    if (parent != null) {
-                        parent.getChildren().remove(itemContainer);
-                    }
-                }
+                e.consume();
+                new Thread(() -> {
+                    boolean success = favoriteDAO.removeFavorite(fav.getUserId(), fav.getBookSlug());
+                    Platform.runLater(() -> {
+                        if (success) {
+                            VBox parent = (VBox) itemContainer.getParent();
+                            if (parent != null) {
+                                parent.getChildren().remove(itemContainer);
+
+                                if (onDeleteCallback != null) onDeleteCallback.run();
+                            }
+                        }
+                    });
+                }).start();
             });
+        }
+    }
+
+    private void openBookDetail() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/read/book_detail.fxml"));
+            Parent root = loader.load();
+
+            BookDetailController detailController = loader.getController();
+            detailController.setBookSlug(currentFav.getBookSlug());
+
+            Stage stage = (Stage) itemContainer.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("WOWTruyen - " + currentFav.getBookName());
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
