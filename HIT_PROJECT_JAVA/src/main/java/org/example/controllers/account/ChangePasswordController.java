3f3ddeb9eb9b24d852e9340be47f0a9acfc6f5bd
Quanle-1112 <lecongquan.dtan@gmail.com>
2026-02-28 10:16:57 +0700

    fix: fix some error

--- HIT_PROJECT_JAVA/src/main/java/org/example/controllers/account/ChangePasswordController.java
+++ HIT_PROJECT_JAVA/src/main/java/org/example/controllers/account/ChangePasswordController.java
@@ -2,7 +2,6 @@ package org.example.controllers.account;
 
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
--- HIT_PROJECT_JAVA/src/main/java/org/example/controllers/admin/AdminDashboardController.java
+++ HIT_PROJECT_JAVA/src/main/java/org/example/controllers/admin/AdminDashboardController.java
@@ -1,7 +1,6 @@
 package org.example.controllers.admin;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
